864a5857102d76a024050068df00f6f7d4ea7900
ahmed <ahmed.max7586@gmail.com>
2022-07-02 17:00:02 -0400

    first commit

---
+++ egbs-p3o2/src/main/java/com/att/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.att.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
---
+++ egbs-p3o2/src/main/java/com/att/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.att.dao;
+
+import java.util.List;
+
+import com.att.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
